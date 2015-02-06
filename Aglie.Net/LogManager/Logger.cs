e42a616d497091323c5dd670dcd0dc155ac04a6a
JailBreak02 <542617413@qq.com>
2015-02-06 11:08:19 +0800

    Log4Net

---
+++ Aglie.Net/LogManager/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogManager
+{
+    /// <summary>
+    /// 记录成功调用的日志对象
+    /// </summary>
+    public class SuccessLogger
+    {
+    }
+
+    /// <summary>
+    /// 记录失败调用
+    /// </summary>
+    public class ErrorLogger
+    {
+
+    }
+}
